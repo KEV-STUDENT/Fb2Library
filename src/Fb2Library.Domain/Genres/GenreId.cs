309e318ad753e3249fd24e583d41b3b2a29dfe9e
KEV <9529777@gmail.com>
2026-06-26 11:03:01 +0300

    Domain + Tests GenreId

---
+++ src/Fb2Library.Domain/Genres/GenreId.cs
@@ -0,0 +1,14 @@
+using Fb2Library.Domain.Shared;
+
+namespace Fb2Library.Domain.Genres
+{
+    public sealed record GenreId : GuidIdentity
+    {
+        public GenreId(Guid value) : base(value) { }
+
+        public static GenreId From(Guid value) => new(value);
+        public static GenreId New() => new(Guid.NewGuid());
+
+        public static implicit operator GenreId(Guid value) => From(value);
+    }
+}
